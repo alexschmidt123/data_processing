7acf04c06676edece2650e22dc9279e9404579ff
Gaoming Lin <gaominglins@gmail.com>
2023-05-21 22:28:01 -0500

    lab

---
+++ distribution_on_PlateReader_pattern.py
@@ -0,0 +1,34 @@
+import csv
+from itertools import islice
+
+# Open the input CSV file and read the data
+with open('/test_file/05202023.csv', 'r') as infile:
+    reader = csv.reader(infile)
+    data = [list(map(float, row)) for row in reader]
+# column numbers for each record
+len_2d = [3,5,7,7,7,5,3]
+
+# convert the record information from data to output_data line by line
+def convert(listA, len_2d):
+    res = iter(listA)
+    return [list(islice(res,i)) for i in len_2d]
+output_data=list()
+for row in data:
+    res = [convert(row, len_2d)]
+    output_data.append(res)
+
+# modify records line by line to fit plate reader scan pattern
+for row in output_data:
+    for i in range(0, 7):
+        s = len(row[0][i])
+        diff = int((7-s)/2);
+        for j in range (0,diff):
+            row[0][i].insert(0," ")
+            row[0][i].append(" ")
+            
+# Open the output CSV file and write the output data
+with open('/test_file/05202023_output.csv', 'w', newline='') as outfile:
+    writer = csv.writer(outfile)
+    for row in output_data:
+        writer.writerows(row[0])
+        writer.writerows("  ")
---
+++ sort_re_average.py
@@ -0,0 +1,16 @@
+import csv
+
+# Open the input CSV file and read the data
+with open('/test_file/05202023.csv', 'r') as infile:
+    reader = csv.reader(infile)
+    data = [list(map(float, row)) for row in reader]
+
+# Sort each row and calculate the average of the 6 largest numbers
+for row in data:
+    row.sort(reverse=True)
+    row.append(sum(row[:6]) / 6)
+
+# Open the output CSV file and write the data
+with open('/test_file/05202023_output.csv', 'w', newline='') as outfile:
+    writer = csv.writer(outfile)
+    writer.writerows(data)
