e99f7eb1b6f0789b977145982f3204946ff4d01b
Gaoming Lin <gaominglins@gmail.com>
2023-05-24 23:37:23 -0500

    kln

--- curve_fitting.py
+++ curve_fitting.py
@@ -9,7 +9,7 @@ x_data = np.array([ 20,16,8,6,4,1,0 ])
 y_data = np.array([ 179,126,122,117,73,55,44])
 
 def model_f(x,F,R,K):
-    return 40+(F-40)*(R+K+x-math.sqrt((R+K+x)^2-4*R*x))/(2*R)
+    return 40+(F-40)*(R+K+x-math.sqrt((R+K+x)**2-4*R*x))/(2*R)
 
 popt, pcov = curve_fit(model_f, x_data, y_data, p0=[179,1,1])
 # a_opt, b_opt, c_opt = popt
