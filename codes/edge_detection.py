5d023b7eef8e0f203e72e24c8f8a50634886d262
Gaoming Lin <gaominglins@gmail.com>
2023-08-02 15:02:03 -0500

    gig

--- codes/edge_detection.py
+++ codes/edge_detection.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 import math
-from matplotlib import pyplot as plt
+from matplotlib import image, pyplot as plt
 
 def euclidean_distance(point1, point2):
     return math.sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2)
@@ -25,43 +25,53 @@ def plot_distribution(data_array):
     plt.ylabel('Frequency') 
     plt.xticks([np.min(data_array),np.mean(data_array),np.max(data_array)])
 
-img = cv2.imread('test_img/front/Phosp-1hr-front-02.tif', 0)
+img_path='test_img/back/Phosp-1hr-back-02.tif'
+img = cv2.imread(img_path, 0)
 content_img = img[0:512, 0:512]
-text_img = img[513:572,0:512]
+text_img = img[513:550,257:423]
+scale=cv2.threshold(text_img, 100, 255, cv2.THRESH_BINARY)[1]
+pixels=np.argwhere(scale == 255)
+max_x = 1
+min_x = 0
+for pixel in pixels:
+    if pixel[0]>max_x:
+        max_x=pixel[1]
+    if pixel[0]<min_x:
+        min_x=pixel[1]
+scale_bar=max_x-min_x
 kernel = np.ones((3,3), np.uint8)
 content_blur = cv2.GaussianBlur(content_img, (3,3), 0)
-img_binary = cv2.threshold(content_blur, 155, 255, cv2.THRESH_BINARY)[1]
-edges = cv2.Canny(img_binary,100,200)
-dilate = cv2.dilate(edges,kernel,iterations=1)
-
-cnts = cv2.findContours(dilate, cv2.RETR_LIST,
+edges = cv2.Canny(content_blur,100,200)
+close = cv2.morphologyEx(edges,cv2.MORPH_CLOSE,kernel,iterations=3)
+result = cv2.morphologyEx(edges,cv2.MORPH_CLOSE,kernel,iterations=3)
+cnts = cv2.findContours(result, cv2.RETR_LIST,
                     cv2.CHAIN_APPROX_SIMPLE)[-2]
-# filter by area
 for cnt in cnts:
-    cv2.drawContours(dilate,[cnt], 0, (255,255,255), -1)
+    cv2.drawContours(result,[cnt], 0, (255,255,255), -1)
 
 minArea = 10
 xcnts = []
 xcnts_area = []
 xcnts_distance = []
 for cnt in cnts:
-    if minArea<cv2.contourArea(cnt):
+    if minArea<len(cnt):
         x, y = cnt.mean(axis=0)[0]
         xcnts.append([x,y])
-        xcnts_area.append(cv2.contourArea(cnt))
-    else:
-        cv2.drawContours(dilate,[cnt], 0, (0,0,0), -1)
+        xcnts_area.append(len(cnt))
+        print(len(cnt))
 xcnts_closest_points=find_closest_points(xcnts)
 for i in range(len(xcnts)):
     xcnts_distance.append(euclidean_distance(xcnts[i],xcnts_closest_points[i]))
 plt.figure('result')
-plt.subplot(221),plt.imshow(content_img,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(222),plt.imshow(dilate,cmap = 'gray')
+plt.subplot(231),plt.imshow(content_img,cmap = 'gray')
+plt.title('Original Image (%s)'%(img_path)), plt.xticks([]), plt.yticks([])
+plt.subplot(232),plt.imshow(result,cmap = 'gray')
 plt.title('Holes'), plt.xticks([]), plt.yticks([])
-plt.subplot(223),
-plot_distribution(xcnts_area)
+plt.subplot(233),plt.imshow(text_img,cmap = 'gray')
+plt.text(0,50,'Scale bar length is %s pixels'%(scale_bar),style='italic',color='red', fontsize=8)
+plt.title('Scale'), plt.xticks([]), plt.yticks([])
+plt.subplot(234),plot_distribution(xcnts_area)
 plt.title('Area')
-plt.subplot(224),plot_distribution(xcnts_distance)
+plt.subplot(235),plot_distribution(xcnts_distance)
 plt.title('Distance')
 plt.show()
--- codes/textDetection.py
+++ codes/textDetection.py
@@ -1,66 +1,25 @@
 # Import required packages
 import cv2
 import pytesseract
-# Homebrew is required if you want to run pytesseract on MacOS machine.
-# Mention the installed location of Tesseract-OCR in your system
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-
-# Read image from which text needs to be extracted
-img = cv2.imread("test_img/hole1.tif")
-
-# Preprocessing the image starts
-
-# Convert the image to gray scale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# Performing OTSU threshold
-ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
-
-# Specify structure shape and kernel size.
-# Kernel size increases or decreases the area
-# of the rectangle to be detected.
-# A smaller value like (10, 10) will detect
-# each word instead of a sentence.
-rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
-
-# Applying dilation on the threshold image
-dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
-
-# Finding contours
-contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
-												cv2.CHAIN_APPROX_NONE)
-
-# Creating a copy of image
-im2 = img.copy()
-
-# A text file is created and flushed
-file = open("output/recognized.txt", "w+")
-file.write("")
-file.close()
-
-# Looping through the identified contours
-# Then rectangular part is cropped and passed on
-# to pytesseract for extracting text from it
-# Extracted text is then written into the text file
-for cnt in contours:
-	x, y, w, h = cv2.boundingRect(cnt)
-	
-	# Drawing a rectangle on copied image
-	rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
-	
-	# Cropping the text block for giving input to OCR
-	cropped = im2[y:y + h, x:x + w]
-	
-	# Open the file in append mode
-	file = open("output/recognized.txt", "a")
-	
-	# Apply OCR on the cropped image
-	text = pytesseract.image_to_string(cropped)
-	
-	# Appending the text into file
-	file.write(text)
-	file.write("\n")
-	
-	# Close the file
-	file.close
+def textDetection(img):
+	# Homebrew is required if you want to run pytesseract on MacOS machine with m chip.
+	# Mention the installed location of Tesseract-OCR in your system
+	pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+	ret, thresh1 = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+	rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
+	dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
+	contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
+													cv2.CHAIN_APPROX_NONE)
+	im2 = img.copy()
+	result=[]
+	for cnt in contours:
+		x, y, w, h = cv2.boundingRect(cnt)
+		# Drawing a rectangle on copied image
+		rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+		# Cropping the text block for giving input to OCR
+		cropped = im2[y:y + h, x:x + w]
+		# Open the file in append mode
+		# Apply OCR on the cropped image
+		text = pytesseract.image_to_string(cropped)
+		result.append(text)
