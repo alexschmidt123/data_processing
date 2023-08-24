d87c21d4133b1ad4430b7de187eb63117301697d
Gaoming Lin <gaominglins@gmail.com>
2023-08-23 19:53:15 -0500

    re

--- codes/time_domain_binding.py
+++ codes/time_domain_binding.py
@@ -1,22 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
 # temperature is 25°C
 # time unit is minute, R_tot and L_tot's concentration unit are both M
 dt = 0.1*1/60
-n = 20000
+# n is the time we repeat this loop
+n = 100
 # k_on unit is M^(-1)min^(-1), k_off unit is min^(-1)
 k_on = 10**7
 k_off = 0.033
 R_tot = 30*10**(-9)
 L_tot = 30*10**(-9)
+data = np.zeros((n,))
+rl = 0
 
-def binding_rate_vs_time(rl):
-    return k_on*rl**2-(k_on*(R_tot+L_tot)+k_off)*rl+k_on*R_tot*L_tot*dt
+# def binding_rate_vs_time(rl):
+#     return k_on*rl**2-(k_on*(R_tot+L_tot)+k_off)*rl+k_on*R_tot*L_tot*dt
 
-def recursion(num):
-    if num == 0:
-        return 0
-    else:
-        return recursion(num-1)+binding_rate_vs_time(recursion(num-1))
+# def recursion(num):
+#     if num == 0:
+#         return 0
+#     else:
+#         return recursion(num-1)+binding_rate_vs_time(recursion(num-1))
 
-data=[]
 for index in range(n):
-    data.append([index,recursion(index)])
+    if index == 0:
+        rl = 0
+        data[index]=rl
+    else:
+        rl = rl + k_on*rl**2-(k_on*(R_tot+L_tot)+k_off)*rl+k_on*R_tot*L_tot*dt
+        data[index]=rl
+
+
+print(data)
+
+t = np.arange(0, n, 1) 
+plt.rcParams["figure.figsize"] = [7.50, 3.50]
+plt.rcParams["figure.autolayout"] = True
+plt.title("RL concentration v.s. time")
+plt.plot(t, data, color="red")
+
+plt.show()
+
